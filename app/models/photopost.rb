58cde74d1349b142c08f3d092e4b0e16969db028
Rafael Flores <rafael.flores@ensobusiness.com>
2016-04-26 13:06:41 -0400

    Changes to the paperclip defaults

--- app/models/photopost.rb
+++ app/models/photopost.rb
@@ -6,7 +6,8 @@ class Photopost < ActiveRecord::Base
   has_attached_file :image, styles: {
       medium: '300x300>',
       thumb: '100x100'
-  }
+  },
+    :url => ":s3_domain_url"
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
