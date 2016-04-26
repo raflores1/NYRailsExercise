51ef3be6c89dd9e2bff998c4a89865fa7c73a3d1
Rafael Flores <rafael.flores@ensobusiness.com>
2016-04-26 11:30:38 -0400

    Changes  to the hostname for s3 again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,9 +84,9 @@ Rails.application.configure do
       :s3_host_name => ENV['S3_HOST_NAME'],
       :s3_credentials => {
           :s3_region =>  ENV['AWS_REGION'],
-          :bucket => ENV['S3_BUCKET_NAME'],
           :access_key_id  => ENV['AWS_ACCESS_KEY_ID'],
           :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-      }
+      },
+      :bucket => ENV['S3_BUCKET_NAME']
   }
 end
