144d60e7ac33f88677ac7d208ae3b0546e20f701
Rafael Flores <rafael.flores@ensobusiness.com>
2016-04-26 18:37:38 -0400

    Changes to eliminate S3

--- config/initializers/paperclip.rb
+++
@@ -1,7 +0,0 @@
-
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-Paperclip::Attachment.default_options[:bucket] = 'us-west-1'
-Paperclip::Attachment.default_options[:credentials] = {:access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']}
-Paperclip::Attachment.default_options[:s3_region] =  'us-west-1'
-Paperclip::Attachment.default_options[:s3_host_name] =  ENV['S3_HOST_NAME']
